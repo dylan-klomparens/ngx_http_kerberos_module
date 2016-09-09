3e97b632a99a7cad8615b9c96d7dc63c036fd3ee
root <root@kerberos.nist.gov>
2016-09-09 16:00:22 -0400

    Fixed a bug that was incorrectly decoding the service ticket. An extra null-terminated byte was present at the end of the service ticket, causing context acceptance to fail. Context acceptance now works properly. Keytab location can now be specified using the "Keytab" directive in an Apache config file. For now this is global, but will be Directory/Location specific eventually. Improved the error print function to output more useful information. Keytab is now read in using gss_acquire_cred_from() rather than setting the keytab environment variable KRB5_KTNAME. This is the cleaner way to do it.

--- mod_kerberos.c
+++ mod_kerberos.c
@@ -1,7 +1,7 @@
 // Package dependancies: apr-devel apr-util-devel krb5-devel
 // Link with: libapr-1.so libaprutil-1.so libgssapi_krb5.so
 
-#include <gssapi/gssapi.h>
+#include <gssapi/gssapi_krb5.h>
 #include <httpd.h>
 #include <http_config.h>
 #include <apr_hooks.h>
@@ -11,21 +11,68 @@
 
 #include <stdio.h>
 
-static void decode_error(FILE* output, OM_uint32 error, int type)
+static const char* keytab_location;
+
+const char* set_keytab_location(cmd_parms* command, void* configuration, const char* argument)
 {
-	OM_uint32 major_status = 0;
+	keytab_location = argument;
+	// TODO: test that keytab is readable.
+	return NULL;
+}
+
+static const command_rec directives[] = {
+	AP_INIT_TAKE1("Keytab", set_keytab_location, NULL, OR_AUTHCFG, "The absolute file path of the web server's Kerberos keytab."),
+	{ NULL }
+};
+
+static void decode_error(FILE* output, char* title, OM_uint32 major_error, OM_uint32 minor_error)
+{
+	if(title)
+		fprintf(output, "%s", title);
+	if(major_error == GSS_S_COMPLETE)
+	{
+		fprintf(output, "Success\n");
+		return;
+	}
 	OM_uint32 minor_status = 0;
 	gss_buffer_desc status_string = GSS_C_EMPTY_BUFFER;
 	OM_uint32 context = 0;
+	// Only check the major error if it isn't GSS_S_FAILURE. Running gss_display_status
+	// on a major error code of GSS_S_FAILURE produces a completely useless error message.
+	if(major_error != GSS_S_FAILURE)
+	{
+		do {
+			gss_display_status(&minor_status, major_error, GSS_C_GSS_CODE, GSS_C_NULL_OID, &context, &status_string);
+			fprintf(output, "GSS error: %s\n", (char*)status_string.value);
+			gss_release_buffer(&minor_status, &status_string);
+		} while(context);
+	}
 	do {
-		major_status = gss_display_status(&minor_status, error, type, GSS_C_NULL_OID, &context, &status_string);
-		if(type == GSS_C_GSS_CODE)
-			fprintf(output, "GSS ");
-		if(type == GSS_C_MECH_CODE)
-			fprintf(output, "Kerberos ");
-		fprintf(output, "error: %s\n", (char*)status_string.value);
+		gss_display_status(&minor_status, minor_error, GSS_C_MECH_CODE, GSS_C_NULL_OID, &context, &status_string);
+		fprintf(output, "Kerberos error: %s\n", (char*)status_string.value);
 		gss_release_buffer(&minor_status, &status_string);
 	} while(context);
+}
+
+static gss_cred_id_t read_keytab(FILE* f)
+{
+	OM_uint32 major_status = 0;
+	OM_uint32 minor_status = 0;
+
+	gss_key_value_element_desc keytab;
+	keytab.key = "keytab";
+	keytab.value = keytab_location;
+
+	gss_key_value_set_desc credential_store;
+	credential_store.count = 1;
+	credential_store.elements = &keytab;
+
+	gss_cred_id_t server_credentials = GSS_C_NO_CREDENTIAL;
+	major_status = gss_acquire_cred_from(&minor_status, GSS_C_NO_NAME, GSS_C_INDEFINITE, GSS_C_NO_OID_SET, GSS_C_ACCEPT, &credential_store, &server_credentials, NULL, NULL);
+
+	decode_error(f, "Read keytab result:\n", major_status, minor_status);
+
+	return server_credentials;
 }
 
 static int kerberos_handler(request_rec* request)
@@ -60,22 +107,25 @@ static int kerberos_handler(request_rec* request)
 
 	// Decode the base 64 service ticket.
 	gss_buffer_desc service_ticket = GSS_C_EMPTY_BUFFER;
-	service_ticket.length = apr_base64_decode_len(tokens[1]);
-	service_ticket.value = apr_pcalloc(request->pool, service_ticket.length);
-	apr_base64_decode(service_ticket.value, tokens[1]);
+	service_ticket.value = apr_pcalloc(request->pool, apr_base64_decode_len(tokens[1]));
+	service_ticket.length = apr_base64_decode(service_ticket.value, tokens[1]);
 
 	OM_uint32 minor_status = 0;
 	gss_ctx_id_t context = GSS_C_NO_CONTEXT;
 	gss_name_t client_name = GSS_C_NO_NAME;
 	gss_buffer_desc response = GSS_C_EMPTY_BUFFER;
-	putenv("KRB5_KTNAME=/etc/krb5.keytab");
+	gss_cred_id_t server_credentials = read_keytab(f);
+
+	if(server_credentials == GSS_C_NO_CREDENTIAL)
+	{
+		fclose(f);
+		return HTTP_UNAUTHORIZED;
+	}
 
 	// Verify the user's credentials.
-	OM_uint32 major_status = gss_accept_sec_context(&minor_status, &context, GSS_C_NO_CREDENTIAL, &service_ticket, GSS_C_NO_CHANNEL_BINDINGS, &client_name, NULL, &response, NULL, NULL, NULL);
+	OM_uint32 major_status = gss_accept_sec_context(&minor_status, &context, server_credentials, &service_ticket, GSS_C_NO_CHANNEL_BINDINGS, &client_name, NULL, &response, NULL, NULL, NULL);
 
-	fprintf(f, "Accept security context result:\n");
-	decode_error(f, major_status, GSS_C_GSS_CODE);
-	decode_error(f, minor_status, GSS_C_MECH_CODE);
+	decode_error(f, "Accept security context result:\n", major_status, minor_status);
 
 	gss_delete_sec_context(&minor_status, &context, GSS_C_NO_BUFFER);
 	gss_release_name(&minor_status, &client_name);
@@ -86,7 +136,7 @@ static int kerberos_handler(request_rec* request)
 	if(major_status != GSS_S_COMPLETE)
 	{
 		gss_release_buffer(&minor_status, &response);
-		return HTTP_CONFLICT; // Currently set for debugging. Change back to HTTP_UNAUTHORIZED
+		return HTTP_UNAUTHORIZED;
 	}
 
 	// The user has successfully authenticated when this point is reached.
@@ -97,7 +147,7 @@ static int kerberos_handler(request_rec* request)
 	const char* header = apr_pstrcat(request->pool, "Negotiate ", encoded_response, NULL);
 	apr_table_set(request->err_headers_out, "WWW-Authenticate", header);
 	gss_release_buffer(&minor_status, &response);
-	return OK;
+	return DECLINED; // TODO: figure out if we need to return OK or DECLINED. Ask Apache community.
 }
 
 static void register_hooks(apr_pool_t* pool)
@@ -112,6 +162,6 @@ module AP_MODULE_DECLARE_DATA kerberos_module =
 	NULL,
 	NULL,
 	NULL,
-	NULL,
+	directives,
 	register_hooks
 };
